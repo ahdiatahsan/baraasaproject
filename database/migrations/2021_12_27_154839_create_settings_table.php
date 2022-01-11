2ca6ec9045498ab712428e80a53eedd53553f056
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-01-12 02:54:11 +0800

    add migrations

---
+++ database/migrations/2021_12_27_154839_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('instagram');
+            $table->string('spotify');
+            $table->string('anchor');
+            $table->string('recruitment_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
