1a76da46ec3456d06f4458206ffa8aa1f1588a1f
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-01 05:43:21 +0800

    edit web routes (require permission)

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+# Auth routes
+require __DIR__ . '/auth.php';
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+# Public routes
+require __DIR__ . '/permission/public.php';
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+# Administrator routes
+require __DIR__ . '/permission/administrator.php';
