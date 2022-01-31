a3e2f1bff8322c6b36dbc22051d3c5d2b10324ab
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-01 06:33:30 +0800

    edit dismiss icon color

--- resources/views/components/administrator/alert/success.blade.php
+++ resources/views/components/administrator/alert/success.blade.php
@@ -15,7 +15,7 @@
     </div>
 
     <button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
-        <span class="svg-icon svg-icon-1 svg-icon-success">
+        <span class="svg-icon svg-icon-1 svg-icon-default">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
                 <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
