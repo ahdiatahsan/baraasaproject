e6190428ed3f19442d13c981859e92596ca5f844
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-01 05:40:21 +0800

    add widgets to global js

--- resources/views/components/administrator/script.blade.php
+++ resources/views/components/administrator/script.blade.php
@@ -1,2 +1,3 @@
 <script src="{{ asset('plugins/global/plugins.bundle.js') }}"></script>
 <script src="{{ asset('js/scripts.bundle.js') }}"></script>
+<script src="{{ asset('js/custom/widgets.js') }}"></script>
