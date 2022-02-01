7b585ee3142b1571c20037c6cb2ea86e6c1e222b
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-01 16:09:38 +0800

    change posts to blogs

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,11 +52,11 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ]; 
+    ];
 
-    public function posts()
+    public function blogs()
     {
-        return $this->hasMany(Post::class);
+        return $this->hasMany(Blog::class);
     }
 
     public function events()
