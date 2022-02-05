5c7ecf614cdc8fe4eadabfb6c4291eb50dbfc498
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-05 16:53:56 +0800

    add: division modul resources

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Division;
-use App\Http\Requests\StoreDivisionRequest;
-use App\Http\Requests\UpdateDivisionRequest;
+use App\Models\Position;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Yajra\DataTables\DataTables;
 
 class DivisionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['role:super_administrator|administrator|member']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +22,7 @@ class DivisionController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.division.index');
     }
 
     /**
@@ -25,18 +32,26 @@ class DivisionController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.division.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreDivisionRequest  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreDivisionRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|unique:divisions,name',
+        ]);
+
+        Division::create([
+            'name' => $request->input('name')
+        ]);
+
+        return redirect()->route('division.create')->with('success', 'Divisi ' . $request->input('name') . ' telah ditambahkan.');
     }
 
     /**
@@ -45,9 +60,20 @@ class DivisionController extends Controller
      * @param  \App\Models\Division  $division
      * @return \Illuminate\Http\Response
      */
-    public function show(Division $division)
+    public function show(Division $division, Request $request)
     {
-        //
+        if ($request->ajax()) {
+            $positions = Position::where('division_id', '=', $division->id)->get();
+
+            return DataTables::of($positions)
+                ->addColumn('action', function ($position) {
+                    return view('dashboard.division.action_show', compact('position'))->render();
+                })
+                ->addIndexColumn()
+                ->make(true);
+        }
+
+        return view('dashboard.division.show', compact('division'));
     }
 
     /**
@@ -58,19 +84,26 @@ class DivisionController extends Controller
      */
     public function edit(Division $division)
     {
-        //
+        return view('dashboard.division.edit', compact('division'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateDivisionRequest  $request
+     * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Division  $division
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateDivisionRequest $request, Division $division)
+    public function update(Request $request, Division $division)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|unique:divisions,name,' . $division->id,
+        ]);
+
+        $division->name = $request->input('name');
+        $division->save();
+
+        return redirect()->route('division.edit', $division->id)->with('success', 'Divisi berhasil diubah.');
     }
 
     /**
@@ -81,6 +114,35 @@ class DivisionController extends Controller
      */
     public function destroy(Division $division)
     {
-        //
+        Session::flash('success', 'Divisi ' . $division->name . ' telah dihapus.');
+
+        $division->delete();
+    }
+
+    /**
+     * Direct to position create with division id
+     */
+    public function position($division)
+    {
+        $division_id = Division::where('id', '=', $division)->get();
+
+        return view('dashboard.position.create', compact('division_id'));
+    }
+
+    /**
+     * Yajra datatable
+     */
+    public function datatable(Request $request)
+    {
+        if ($request->ajax()) {
+            $divisions = Division::all();
+
+            return DataTables::of($divisions)
+                ->addColumn('action', function ($division) {
+                    return view('dashboard.division.action', compact('division'))->render();
+                })
+                ->addIndexColumn()
+                ->make(true);
+        }
     }
 }
