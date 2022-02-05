1d658ab097b28286400db14628e132ee9e62bccc
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-05 16:52:51 +0800

    modified error and success alert component

--- resources/views/components/dashboard/alert/error.blade.php
+++ resources/views/components/dashboard/alert/error.blade.php
@@ -1,16 +1,15 @@
 @if ($errors->any())
-<div class="alert alert-dismissible bg-light-danger d-flex flex-column flex-sm-row w-100 py-5 px-5 mb-5">
-    <span class="svg-icon svg-icon-2hx svg-icon-danger me-4 mb-5 mb-sm-0">
+<div class="alert alert-danger d-flex align-items-center p-5 mb-10">
+    <span class="svg-icon svg-icon-2hx svg-icon-danger me-4">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="10" fill="black">
-            </rect>
-            <rect x="11" y="14" width="7" height="2" rx="1" transform="rotate(-90 11 14)" fill="black"></rect>
-            <rect x="11" y="17" width="2" height="2" rx="1" transform="rotate(-90 11 17)" fill="black"></rect>
+            <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="10" fill="black" />
+            <rect x="7" y="15.3137" width="12" height="2" rx="1" transform="rotate(-45 7 15.3137)" fill="black" />
+            <rect x="8.41422" y="7" width="12" height="2" rx="1" transform="rotate(45 8.41422 7)" fill="black" />
         </svg>
     </span>
 
-    <div class="d-flex flex-column pe-0 pe-sm-10">
-        <h4 class="fw-bold">Terjadi Kesalahan</h4>
+    <div class="d-flex flex-column">
+        <h4 class="mb-1 text-danger">Terjadi Kesalahan!</h4>
         <span>
             <ul>
                 @foreach ($errors->all() as $error)
@@ -22,13 +21,7 @@
 
     <button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto"
         data-bs-dismiss="alert">
-        <span class="svg-icon svg-icon-1 svg-icon-default">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)"
-                    fill="black" />
-                <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
-            </svg>
-        </span>
+        <i class="bi bi-x fs-1 text-danger"></i>
     </button>
 </div>
 @endif
