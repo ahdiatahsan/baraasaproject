de8bcbde98e36b86200eae1b0b1d99524ab4f39d
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-07 02:47:55 +0800

    fix: add create function

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -27,6 +27,16 @@ class PositionController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard.position.create');
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request $request
