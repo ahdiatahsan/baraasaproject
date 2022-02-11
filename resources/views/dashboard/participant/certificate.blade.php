576c52ed16b4a620243f9f45b877aee6ce68a045
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-11 23:45:40 +0800

    fix: certificate status

--- resources/views/dashboard/participant/certificate.blade.php
+++ resources/views/dashboard/participant/certificate.blade.php
@@ -1,5 +1,5 @@
-@if ($participant->certificate != null)
-<span class="badge badge-light-success">Ada</span>
+@if ($participant->certificate == null)
+<span class="badge badge-light-danger">Tidak Ada</span>
 @else
-<span class="badge badge-light-danger">Belum Ada</span>
+<span class="badge badge-light-success">Ada</span>
 @endif
