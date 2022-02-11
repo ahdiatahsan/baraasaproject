76508555f901d3ac8968daca9e816bc20b9753e4
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-11 23:45:16 +0800

    fix: status text

--- resources/views/dashboard/participant/status.blade.php
+++ resources/views/dashboard/participant/status.blade.php
@@ -1,5 +1,5 @@
 @if ($participant->data_status == 0)
 <span class="badge badge-light-danger">Belum Diverifikasi</span>
 @else
-<span class="badge badge-light-success">Diverifikasi</span>
+<span class="badge badge-light-success">Telah Diverifikasi</span>
 @endif
