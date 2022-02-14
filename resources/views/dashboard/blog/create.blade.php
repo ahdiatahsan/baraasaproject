83d1e77cb992562ec49fdec34ae6d1536eb90b7f
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-02-15 03:19:19 +0800

    fix: edit tinymce menu

--- resources/views/dashboard/blog/create.blade.php
+++ resources/views/dashboard/blog/create.blade.php
@@ -102,6 +102,16 @@
         <script>
             tinymce.init({
 			selector: '#body',
+            menu: {
+                file: { title: 'File', items: 'newdocument restoredraft | preview | print ' },
+                edit: { title: 'Edit', items: 'undo redo | cut copy paste | selectall | searchreplace' },
+                view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
+                insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
+                format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats align lineheight | forecolor backcolor | removeformat' },
+                tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | code wordcount' },
+                table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
+                help: { title: 'Help', items: 'help' }
+            },
 			height: 500,
 			plugins: [
 				'advlist autolink lists link image charmap print preview anchor',
